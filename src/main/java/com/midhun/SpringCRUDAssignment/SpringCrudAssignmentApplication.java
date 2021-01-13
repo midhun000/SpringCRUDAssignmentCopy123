54411324ea411f144a081e6a71b1b0056bc5efc7
midhun000 <46200426+midhun000@users.noreply.github.com>
2021-01-13 10:48:32 +0530

    Initial commit

---
+++ src/main/java/com/midhun/SpringCRUDAssignment/SpringCrudAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.midhun.SpringCRUDAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/midhun/SpringCRUDAssignment/SpringCrudAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midhun.SpringCRUDAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
